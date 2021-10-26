41327fce9baea28748e074a795eb5f7f51977067
thisismattsmith <hello@thisismattsmith.com>
2021-10-26 18:11:06 +1100

    Going social
    Updated favicon and images for social sharing.

--- src/components/bio.js
+++ src/components/bio.js
@@ -45,9 +45,9 @@ const Bio = () => {
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
+          {` `}. You can reach me on
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          Twitter
           </a>
         </p>
       )}
